a8863cc877c526835b6fe989a50919474f6a002e
Dhayalan <murugesandhayalan609@gmail.com>
2026-09-25 11:42:40 +0530

    chore: initialize Spring Boot backend

---
+++ src/test/java/com/insurance/quotation/InsuranceQuotationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.quotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceQuotationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
